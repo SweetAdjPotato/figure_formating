113067fcd4f78b8dc1d22afd172d1af4acecba7b
Zhaoyun Ma <mazhaoyunyh@gmail.com>
2020-08-03 22:36:27 -0400

    class file added for figure format

---
+++ figure_formating/FormatPlot.py
@@ -0,0 +1,133 @@
+import matplotlib.pyplot as plt
+
+class FormatPlot:
+	""" FormatPlot class to create well-formatted, well labeled visualizations
+		based on matplotlib
+
+	Attributes:
+		None
+
+	"""
+	def __init__(self):
+		x = []
+		y = []
+
+	def read_data_file(self, file_name1, file_name2):
+
+		"""Function to read in data from a txt file. The txt file should have
+		one number (float) per line. The numbers are stored in the data attribute.
+
+		Args:
+			file_name (string): name of a file to read from
+
+		Returns:
+			None
+
+		"""
+
+		with open(file_name1, file_name2) as file1, file2:
+			data_list1 = []
+			line1 = file1.readline()
+			while line1:
+				data_list1.append(int(line1))
+				line1 = file1.readline()
+
+			data_list2 = []
+			line2 = file2.readline()
+			while line2:
+				data_list2.append(int(line2))
+				line2 = file2.readline()
+		file1.close()
+		file2.close()
+
+		x = data_list1
+		y = data_list2
+
+		return x, y
+
+	# def reformat():
+	#
+	# 	"""Function to reformat the plotted figures.
+	#
+	# 	Args:
+	# 		None
+	#
+	# 	Returns:
+	# 		None
+	#
+	# 	"""
+	# 	plt.xticks(fontsize=10)
+	# 	plt.yticks(fontsize=10)
+	# 	plt.box(True)
+	# 	plt.grid(True)
+	# 	plt.show()
+
+
+	def plot(x , y):
+
+		"""Function to calculate the standard deviation of the data set.
+
+		Args:
+			None
+
+		Returns:
+			None
+
+		"""
+
+		plt.figure(figsize = (7,4))
+		plt.plot(x, y, color='blue',linestyle='dashed')
+		plt.title('xy relationship',fontsize=14)
+		plt.ylabel('y', fontsize=12)
+		plt.xlabel('x', fontsize=12)
+		plt.xticks(fontsize=10)
+		plt.yticks(fontsize=10)
+		plt.box(True)
+		plt.grid(True)
+		plt.show()
+		# reformat()
+
+
+
+	def hist(x):
+		"""Function to output a reformatted histogram of the instance variable data using
+		matplotlib pyplot library.
+
+		Args:
+			None
+
+		Returns:
+			None
+		"""
+		plt.hist(x)
+		plt.title('Histogram of Data', fontsize=14)
+		plt.xlabel('data', fontsize=12)
+		plt.ylabel('count', fontsize=12)
+		plt.xticks(fontsize=10)
+		plt.yticks(fontsize=10)
+		plt.box(True)
+		plt.grid(True)
+		plt.show()
+		# reformat()
+
+
+	def bar(x,y):
+		"""Function to output a reformatted bar plot of the instance variable data using
+		matplotlib pyplot library.
+
+		Args:
+			None
+
+		Returns:
+			None
+		"""
+		plt.bar(x,y)
+		plt.title('Histogram of Data', fontsize=14)
+		plt.xlabel('data', fontsize=12)
+		plt.ylabel('count', fontsize=12)
+		plt.xticks(fontsize=10)
+		plt.yticks(fontsize=10)
+		plt.box(True)
+		plt.grid(True)
+		plt.show()
+		# reformat()
