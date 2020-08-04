b7531d2bad342994be6e45810e162fd6af4175f7
Zhaoyun Ma <mazhaoyunyh@gmail.com>
2020-08-04 08:32:08 -0400

    version 0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='figure_formating',
-      version='0.1',
+      version='0.2',
       description='format the plotted figures',
       packages=['figure_formating'],
       author='Zhaoyun Ma',
